c2d225153a657de1d1fe3ee22297863e774eb120
ginzajason <melnikjason@gmail.com>
2020-11-05 05:41:55 -0500

    add

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +70,11 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,27 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import serializers, viewsets, routers
+from rest_framework.authtoken.admin import User
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+
+from quickstart import views
+from django.urls import path, include
+
+# Serializers define the API representation.
+from snippets.views import UserViewSet
+
+
+API_TITLE = 'Pastebin API'
+API_DESCRIPTION = 'A Web API for creating and viewing highlighted code snippets.'
+schema_view = get_schema_view(title=API_TITLE)
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    url(r'^', include('snippets.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^schema/$', schema_view),
+    url(r'^docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION))
 ]
