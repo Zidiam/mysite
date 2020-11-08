62415e349bf7496688bce90e95bd764bdac00df2
ginzajason <melnikjason@gmail.com>
2020-11-07 20:06:38 -0500

    removed quickstart

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,7 +6,6 @@ from rest_framework.authtoken.admin import User
 from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
 
-from quickstart import views
 from django.urls import path, include
 
 # Serializers define the API representation.
